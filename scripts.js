75c5d782f09eecc954c802ea97af260c735480ce
Yan233_ <Yan233th@126.com>
2022-12-23 22:36:47 +0800

    mod

---
+++ scripts.js
@@ -0,0 +1,6 @@
+window.onload = function ()
+{
+    var div1 = document.getElementById ("link");
+    var div2 = document.getElementById ("avatar");
+    div2.style.height = div1.offsetHeight - 40 + "px";
+}
