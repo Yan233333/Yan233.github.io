6fa999f0e5661f3edbbb5090ea5d4a86ad01c5cf
Yan233_ <Yan233th@126.com>
2023-08-24 18:32:47 +0800

    修复了框体不等高的bug，替换背景与头像为中等质量图片

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
-window.onload = function ()
-{
-    var div1 = document.getElementById ("link");
-    var div2 = document.getElementById ("avatar");
-    div2.style.height = div1.offsetHeight - 40 + "px";
+window.onload = function () {
+    // 获取第一个框的高度
+    var firstBox = document.getElementById('link');
+    var firstBoxHeight = firstBox.offsetHeight;
+
+    // 将第一个框的高度应用到第二个框
+    var secondBox = document.getElementById('avatar');
+    secondBox.style.height = firstBoxHeight + 'px';
 }
