67920e4a73b9d591eeff6f18545a066024bebe20
Yan233_ <Yan233th@126.com>
2023-10-11 23:55:52 +0800

    Optimize the timing of script triggers

--- scripts.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    // 获取第一个框的高度
-    var firstBox = document.getElementById('link');
-    var firstBoxHeight = firstBox.offsetHeight;
-
-    // 将第一个框的高度应用到第二个框
-    var secondBox = document.getElementById('avatar');
-    secondBox.style.height = firstBoxHeight + 'px';
-}
